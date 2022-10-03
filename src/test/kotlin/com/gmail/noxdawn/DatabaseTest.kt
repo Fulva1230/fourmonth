f1a5572404afc210d781b6f8d91d3d8b584322dd
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2022-10-03 21:13:24 +0800

    change dependencies

--- src/test/kotlin/com/gmail/noxdawn/DatabaseTest.kt
+++ src/test/kotlin/com/gmail/noxdawn/DatabaseTest.kt
@@ -2,13 +2,13 @@ package com.gmail.noxdawn
 
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import io.vertx.reactivex.pgclient.PgPool
+import io.vertx.kotlin.coroutines.await
+import io.vertx.pgclient.PgPool
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import jakarta.inject.Inject
 import jakarta.inject.Named
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.rx2.await
 
 class DatabaseTest constructor(
     @param:Named("ssl") val client: PgPool
@@ -21,7 +21,7 @@ class DatabaseTest constructor(
         Assertions.assertTrue(application.isRunning)
         runBlocking {
             val to_message_info =
-                client.query("SELECT text, hints FROM message_to WHERE id=1").rxExecute().await()
+                client.query("SELECT text, hints FROM message_to WHERE id=1").execute().await()
             to_message_info.first().get(String::class.java, "text")
             to_message_info.first().getArrayOfStrings("hints")
         }
